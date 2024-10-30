820f5d9bc8eadffb64b1fd17306bce11f8d05d44
Marvin <kern.marvin@outlook.de>
2024-10-31 00:52:36 +0100

    Docker ready

--- src/main/java/de/thi/cnd/ingredient/broker/RabbitMQBrokerServiceImpl.java
+++ src/main/java/de/thi/cnd/ingredient/broker/RabbitMQBrokerServiceImpl.java
@@ -2,6 +2,7 @@ package de.thi.cnd.ingredient.broker;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,13 +10,16 @@ public class RabbitMQBrokerServiceImpl implements BrokerService {
 
     private final RabbitTemplate rabbitTemplate;
 
+    @Value("${app.message.queue.ingredients.exchange}")
+    private String topicExchangeName;
+
     @Autowired
     public RabbitMQBrokerServiceImpl(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
     }
 
     public void sendMessage(String routing_key, String message) {
-        rabbitTemplate.convertAndSend("cnd.ingredients_exchange", routing_key, message);
+        rabbitTemplate.convertAndSend(topicExchangeName, routing_key, message);
     }
 
 }
