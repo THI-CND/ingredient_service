d09b127573dff986bfc45c832e0af26e7be12f49
Marvin <kern.marvin@outlook.de>
2025-01-01 14:48:54 +0100

    feat(service): Implemented gRPC

--- src/main/java/de/thi/cnd/adapter/api/grpc/IngredientGrpcController.java
+++ src/main/java/de/thi/cnd/adapter/api/grpc/IngredientGrpcController.java
@@ -1,9 +1,94 @@
 package de.thi.cnd.adapter.api.grpc;
 
+import de.thi.cnd.domain.IngredientService;
+import de.thi.cnd.domain.model.Ingredient;
+import de.thi.cnd.ingredient.*;
+import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 
-@GrpcService
-public class IngredientGrpcController {
+import java.util.List;
 
+@GrpcService
+public class IngredientGrpcController extends IngredientServiceGrpc.IngredientServiceImplBase {
+
+    private final IngredientService ingredientService;
+
+    public IngredientGrpcController(IngredientService ingredientService) {
+        this.ingredientService = ingredientService;
+    }
+
+    @Override
+    public void getIngredients(Empty request, StreamObserver<IngredientsResponse> responseObserver) {
+        List<Ingredient> ingredients = ingredientService.getIngredients();
+        IngredientsResponse.Builder responseBuilder = IngredientsResponse.newBuilder();
+
+        for (Ingredient ingredient : ingredients) {
+            IngredientResponse ingredientResponse = IngredientResponse.newBuilder()
+                    .setId(ingredient.getId())
+                    .setName(ingredient.getName())
+                    .setUnit(ingredient.getUnit())
+                    .addAllTags(ingredient.getTags())
+                    .build();
+            responseBuilder.addIngredients(ingredientResponse);
+        }
+
+        IngredientsResponse response = responseBuilder.build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getIngredient(IngredientIdRequest request, StreamObserver<IngredientResponse> responseObserver) {
+        Ingredient ingredient = ingredientService.getIngredientById(request.getId());
+        IngredientResponse response = IngredientResponse.newBuilder()
+                .setId(ingredient.getId())
+                .setName(ingredient.getName())
+                .setUnit(ingredient.getUnit())
+                .addAllTags(ingredient.getTags())
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void createIngredient(CreateIngredientRequest request, StreamObserver<IngredientResponse> responseObserver) {
+        Ingredient ingredient = ingredientService.createIngredient(request.getName(), request.getUnit(), request.getTagsList());
+        IngredientResponse response = IngredientResponse.newBuilder()
+                .setId(ingredient.getId())
+                .setName(ingredient.getName())
+                .setUnit(ingredient.getUnit())
+                .addAllTags(ingredient.getTags())
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void updateIngredient(UpdateIngredientRequest request, StreamObserver<IngredientResponse> responseObserver) {
+        Ingredient ingredient = ingredientService.updateIngredient(request.getId(), request.getName(), request.getUnit(), request.getTagsList());
+        IngredientResponse response = IngredientResponse.newBuilder()
+                .setId(ingredient.getId())
+                .setName(ingredient.getName())
+                .setUnit(ingredient.getUnit())
+                .addAllTags(ingredient.getTags())
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void deleteIngredient(IngredientIdRequest request, StreamObserver<Empty> responseObserver) {
+        ingredientService.deleteIngredient(request.getId());
+        responseObserver.onNext(Empty.newBuilder().build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getTags(Empty request, StreamObserver<TagsResponse> responseObserver) {
+        List<String> tags = ingredientService.getTags();
+        TagsResponse response = TagsResponse.newBuilder().addAllTags(tags).build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
 
 }
